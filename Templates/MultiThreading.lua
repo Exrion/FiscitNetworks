5b3c4245ee3277274a0f6454d12e785a181c957e
Titus Lim <titus.happy@gmail.com>
2023-02-14 15:36:42 +0800

    added comments and console outputs

--- Templates/MultiThreading.lua
+++ Templates/MultiThreading.lua
@@ -1,22 +1,37 @@
 -- Coroutine Functions
+print('===== Program Started =====');
 thread = {threads = {}, current = 1}
 
 function thread.create(func)
     local t = {};
+    -- Create coroutine.
     t.co = coroutine.create(func);
+
+    -- Remove thread from thread table.
     function t:stop()
         for i, th in pairs(thread.threads) do
             if th == t then table.remove(thread.threads, i) end
         end
     end
+
+    -- Add thread to thread table.
     table.insert(thread.threads, t);
+    print('Initializing', t.co);
     return t;
 end
 
 function thread:run()
+    print('===== Threads running concurrently: ', #thread.threads, '=====\n');
+
+    -- Thread Loop
     while true do
+        -- If single-threaded, run that thread.
         if #thread.threads < 1 then return end
+
+        -- If multi-threaded, check if current thread exceeds total threads, set to first index if true.
         if thread.current > #thread.threads then thread.current = 1 end
+
+        -- Run current thread then increment current thread counter.
         coroutine.resume(thread.threads[thread.current].co, true);
         thread.current = thread.current + 1;
     end
@@ -25,12 +40,13 @@ end
 function sleep(time) event.pull(time) end
 
 -- Program Functions Here
-
 local function Hello()
     while true do
         -- Execution
         sleep(1);
         print('Hello');
+
+        -- Yield
         coroutine.yield();
     end
 end
@@ -40,6 +56,8 @@ local function World()
         -- Execution
         sleep(1);
         print('World');
+
+        -- Yield
         coroutine.yield();
     end
 end
@@ -49,7 +67,7 @@ local function Main()
     t1 = thread.create(Hello);
     t2 = thread.create(World);
 
-    thread.run()
+    thread.run();
 end
 
 -- Begin Program
